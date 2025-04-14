297f719f745b253728b8605925a5e2116ec9c16d
adrian navarrete <adrian.navarrete@yahoo.com>
2025-04-14 14:56:48 -0700

    Changes in petfinder.rs for later implamentation.

---
+++ src/utils/petfinder.rs
@@ -0,0 +1,16 @@
+use dotenv::dotenv;
+use std::env;
+
+
+fn petfinder() {
+    let client: Client = Client::new();
+    let mut pet: Vec<Value> = Vec::new();
+    let mut page_token: String = String::new();
+
+    loop {
+        let url: String = format{
+            //need to format url for api access
+        };
+    }
+
+}
