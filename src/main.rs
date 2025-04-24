f0bb9b6b3297fa8155a515f2dd18e0a6e11c4b43
adrian navarrete <adrian.navarrete@yahoo.com>
2025-04-23 18:35:28 -0700

    added search bar with button

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use dotenv::dotenv;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     //read and initalize api key/secret.
     dotenv().ok();
+
     let client_id = match std::env::var("API_KEY") {
         Ok(val) => val,
         Err(e) => {
@@ -16,6 +17,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             return Err(e.into());
         }
     };
+
     let client_secret = match std::env::var("API_SECRET") {
         Ok(val) => val,
         Err(e) => {
@@ -50,19 +52,27 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 #[derive(Default)]
 struct Home4PawsApp {
     status_message: String,
+    location: String,
 }
 
 impl Home4PawsApp {
     fn new(_cc: &eframe::CreationContext<'_>, status_message: String) -> Self{
-        Self{ status_message }
+        Self{ 
+            status_message, 
+            location: "City/State or Zip".to_owned(),
+        }
     }
 }
 
 impl eframe::App for Home4PawsApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("🐾 Home4Paws - Adopt a New Friend");
-            ui.label("This will display adoptable pets from an API.");
+
+            ui.heading("Home4Paws - Adopt a New Friend");
+            ui.horizontal(|ui| {
+                ui.text_edit_singleline(&mut self.location);
+                ui.button("Search");
+            });
             ui.separator();
             ui.label(&self.status_message);
         });
